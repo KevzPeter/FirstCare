2f19699fc90bdbd751cecd60a091e06548fdd4ce
Kevin Peter <29401940+KevzPeter@users.noreply.github.com>
2020-08-25 00:14:31 +0530

    Changed algorithm
    Changed parameters of closest hospital/health centre tracking algorithm based on distance rather than prominence, which tended to return results far away from actual location

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -25,11 +25,11 @@ export const Main = () => {
 
                axios.post('/maps',places[0])
                .then(res=>{
-                    places[1]={latitude:res.data.candidates[0].geometry.location.lat,longitude:res.data.candidates[0].geometry.location.lng}
-                    Setname(res.data.candidates[0].name)
-                    Setaddr(res.data.candidates[0].formatted_address)
+                    places[1]={latitude:res.data.results[0].geometry.location.lat,longitude:res.data.results[0].geometry.location.lng}
+                    Setname(res.data.results[0].name)
+                    Setaddr(res.data.results[0].vicinity)
                     const route=[places[0],places[1]]
-                    const place_id={"place_id":res.data.candidates[0].place_id}
+                    const place_id={"place_id":res.data.results[0].place_id}
                     axios.post('/phone',place_id).then(res=>{
                         Setph(res.data.result.formatted_phone_number)
                     }).catch(e=>console.log(e))
